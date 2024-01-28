faa98c83836bfdb56e25d52200f2829c8934c59f
Juan Martin Silva <juanmartinsilva74@gmail.com>
2024-01-28 16:51:29 -0300

    create seed module and import products services on it

---
+++ src/seed/seed.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@nestjs/common';
+
+import { ProductsService } from '../products/products.service';
+
+@Injectable()
+export class SeedService {
+    constructor(private readonly productsService: ProductsService) {}
+
+    async runSeed() {
+        await this.insertNewProducts();
+
+        return 'Seed executed';
+    }
+
+    private async insertNewProducts() {
+        await this.productsService.deleteAllProducts();
+        return true;
+    }
+}
