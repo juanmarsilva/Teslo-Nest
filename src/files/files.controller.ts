2038f9e93670508a3d4966dd6fbaf82f6a52df08
Juan Martin Silva <juanmartinsilva74@gmail.com>
2024-01-29 00:12:29 -0300

    md create files module for save files

---
+++ src/files/files.controller.ts
@@ -0,0 +1,40 @@
+import {
+    BadRequestException,
+    Controller,
+    Post,
+    UploadedFile,
+    UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+
+import { diskStorage } from 'multer';
+
+import { FilesService } from './files.service';
+import { fileFilter } from './helpers/file-filter.helper';
+
+@Controller('files')
+export class FilesController {
+    constructor(private readonly filesService: FilesService) {}
+
+    @Post('product')
+    @UseInterceptors(
+        FileInterceptor('file', {
+            fileFilter: fileFilter,
+            // limits: { fileSize: 1000 }
+            storage: diskStorage({
+                destination: './static/uploads',
+            }),
+        }),
+    )
+    uploadProductImage(@UploadedFile() file: Express.Multer.File) {
+        if (!file) {
+            throw new BadRequestException(
+                'Make sure that the file is an image',
+            );
+        }
+
+        return {
+            fileName: file.originalname,
+        };
+    }
+}
