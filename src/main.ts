b3d3422c7595291820f833098111cbc0839453a0
Juan Martin Silva <juanmartinsilva74@gmail.com>
2024-02-10 18:09:22 -0300

    implement env variable on port

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule, {
         logger: ['error', 'log', 'warn'],
     });
+
+    const logger = new Logger('bootstrap');
 
     app.setGlobalPrefix('api');
 
@@ -16,6 +18,7 @@ async function bootstrap() {
         }),
     );
 
-    await app.listen(3000);
+    await app.listen(process.env.PORT);
+    logger.log(`App running on port ${process.env.PORT}`);
 }
 bootstrap();
